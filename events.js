6ac6e3054cae26f349f72bcd10d6ac4c6fdaaf9b
jahangiralam7864 <wedevs95@gmail.com>
2022-02-09 01:55:16 +0600

    video-5

--- events.js
+++ events.js
@@ -1,5 +1,21 @@
-blueButton.onclick = makeBlue;
+console.log('jahangir alam joy');
+ const student = {name: 'jahangir alam',age : 23, height : '5.7inc'}
+ console.log(document.body);
 
-function makeRed() {
-    document.body.style.backgroundColor='red';
+ const blogTitle = document.getElementsByTagName('h3');
+ for (const h3 of blogTitle) {
+    console.log(h3.innerText);
+ }
+
+const blogDetails = document.getElementsByTagName('p');
+for (const p of blogDetails){
+    console.log(p);
 }
+
+const nameTitle = document.getElementById('names')
+names.style.textAlign = 'center'; 
+
+const blog = document.getElementById('blogs')
+blogs.innerText = 'md jahangir alam';
+blogs.style.backgroundColor ='red'
+blogs.style.padding = '10px'
