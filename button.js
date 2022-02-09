138c32ed05326021f44b080cab8637e4fc731a6a
jahangiralam7864 <wedevs95@gmail.com>
2022-02-09 15:36:37 +0600

    gold button add

--- button.js
+++ button.js
@@ -3,7 +3,18 @@ function makeRed() {
 }
 
 const blueButton = document.getElementById('make-blue');
-        blueButton.onclick = makeblue;
-        function makeblue() {
-            document.body.style.backgroundColor = 'blue';
-        }
+blueButton.onclick = makeblue;
+function makeblue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+const grayButton = document.getElementById('make-gray-button');
+grayButton.onclick = function () {
+    document.body.style.backgroundColor = 'gray';
+}
+
+const goldenButton = document.getElementById('make-golden');
+goldenButton.addEventListener('click',makeGoldenutton);
+function makeGoldenutton() {
+    document.body.style.backgroundColor = 'gold';
+}
